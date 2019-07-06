316f3f5b3729f858146c6f8836b336669080b89a
jredondo <j.credondo@icloud.com>
2019-07-06 16:09:46 +0100

    pass

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+    require('dotenv').config();
 }
 
 const express = require('express');
@@ -12,6 +12,8 @@ const authorsRouter = require('./routes/authors');
 const mongoose = require('mongoose');
 const app = express();
 
+
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
@@ -19,7 +21,9 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
 db.once('open', error => console.log('Connected to Mongoose'));
@@ -27,4 +31,6 @@ db.once('open', error => console.log('Connected to Mongoose'));
 app.use('/', indexRouter);
 app.use('/authors', authorsRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log('Server Running');
+});
